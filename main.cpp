31628b2502963c8e34bc71265b97a2306c8ce3a8
George Yakushev <yakushev.ga@phystech.edu>
2023-10-17 19:10:08 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+class
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
